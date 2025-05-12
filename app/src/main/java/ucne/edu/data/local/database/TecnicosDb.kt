e6b8365a51ac6026edcaacc6288c4583fffadcdf
Yohualkis <Yohualkis99@gmail.com>
2025-05-11 23:53:35 -0400

    Se creo la base de datos (migracion nueva incluida)

---
+++ app/src/main/java/ucne/edu/data/local/database/TecnicosDb.kt
@@ -0,0 +1,15 @@
+package ucne.edu.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import ucne.edu.data.local.dao.TecnicoDao
+import ucne.edu.data.local.entities.TecnicoEntity
+
+@Database(
+    entities = [TecnicoEntity::class],
+    version = 2,
+    exportSchema = false
+)
+abstract class TecnicosDb : RoomDatabase(){
+    abstract fun TecnicoDao(): TecnicoDao
+}
