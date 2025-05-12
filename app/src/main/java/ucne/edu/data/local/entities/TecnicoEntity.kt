03c17477fe2b8ccb5e0fcc0836c64d2a0714310f
Yohualkis <Yohualkis99@gmail.com>
2025-05-11 23:53:13 -0400

    Se creo la entidad

---
+++ app/src/main/java/ucne/edu/data/local/entities/TecnicoEntity.kt
@@ -0,0 +1,12 @@
+package ucne.edu.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Tecnicos")
+data class TecnicoEntity(
+    @PrimaryKey
+    val tecnicoId: Int?,
+    val nombre: String,
+    val sueldoHora: Double
+)
