6ebfb8b87a2b66c85761a2b500567f15fd6f6eef
Yohualkis <Yohualkis99@gmail.com>
2025-05-22 09:50:56 -0400

    Se modifico el listado de tecnicos

--- app/src/main/java/ucne/edu/presentation/tecnicos/DesplegarListadoDeTecnicos.kt
+++ app/src/main/java/ucne/edu/presentation/tecnicos/DesplegarListadoDeTecnicos.kt
@@ -1,7 +1,5 @@
 package ucne.edu.presentation.tecnicos
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -10,21 +8,18 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Card
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import ucne.edu.data.local.entities.TecnicoEntity
+import ucne.edu.presentation.componentes.TopBarGenerica
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -32,69 +27,48 @@ fun DesplegarListadoDeTecnicos(
     listaTecnicos: List<TecnicoEntity>,
     onEditClick: (Int?) -> Unit,
     onDeleteClick: (TecnicoEntity) -> Unit,
-    tecnicoViewModel: TecnicosViewModel?
+    tecnicoViewModel: TecnicosViewModel?,
+    navController: NavController,
 ) {
     Scaffold(
-
         floatingActionButton = {
             FloatingActionButton(
                 onClick = {
-                    println("Navegando a técnico con ID 0")
                     onEditClick(0)
                 }
             ) {
                 Icon(Icons.Filled.Add, "Agregar")
             }
         },
+        topBar = {
+            TopBarGenerica(
+                navController = navController,
+                titulo = "Listado de Técnicos"
+            )
+        }
     ) { innerPadding ->
-        Column {
-            Card(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(innerPadding)
-            ) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp)
-                ) {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 16.dp)
-                    ) {
-                        Text(
-                            text = "Listado de Técnicos",
-                            fontSize = 20.sp,
-                            modifier = Modifier.align(Alignment.Center)
-                        )
-                    }
-                    Spacer(modifier = Modifier.height(16.dp))
-
-                    LazyColumn(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(8.dp)
-                    ) {
-                        items(listaTecnicos) { tecnico ->
-                            TecnicoCardInfo(
-                                tecnico = tecnico,
-                                onEditClick = { onEditClick(tecnico.tecnicoId) },
-                                onDeleteClick = { onDeleteClick(tecnico) },
-                                tecnicoViewModel = tecnicoViewModel
-                            )
-                            Spacer(modifier = Modifier.height(8.dp))
-                        }
-                    }
-                }
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+                .padding(innerPadding)
+        ) {
+            items(listaTecnicos) { tecnico ->
+                TecnicoCardInfo(
+                    tecnico = tecnico,
+                    onEditClick = { onEditClick(tecnico.tecnicoId) },
+                    onDeleteClick = { onDeleteClick(tecnico) },
+                    viewModel = tecnicoViewModel
+                )
+                Spacer(modifier = Modifier.height(8.dp))
             }
         }
     }
 }
 
-@Preview(showBackground = true, showSystemUi = true)
+@Preview(showBackground = true)
 @Composable
-fun Preview(){
+fun Preview() {
     val listaTecnicos = listOf(
         TecnicoEntity(
             tecnicoId = 1,
@@ -112,10 +86,12 @@ fun Preview(){
             sueldoHora = 200.0
         )
     )
+    val nav: NavController = rememberNavController()
     DesplegarListadoDeTecnicos(
         listaTecnicos = listaTecnicos,
         onEditClick = {},
         onDeleteClick = {},
-        tecnicoViewModel = null
+        tecnicoViewModel = null,
+        navController = nav
     )
 }
