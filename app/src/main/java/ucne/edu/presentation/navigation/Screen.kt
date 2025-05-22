cf866da440f83e2b2458120aefa656950acff082
Yohualkis <Yohualkis99@gmail.com>
2025-05-22 09:57:49 -0400

    Se agregaron las data classes y objects que representan las pantallas navegables que se unen al grafo de navegacion

--- app/src/main/java/ucne/edu/presentation/navigation/Screen.kt
+++ app/src/main/java/ucne/edu/presentation/navigation/Screen.kt
@@ -4,8 +4,23 @@ import kotlinx.serialization.Serializable
 
 sealed class Screen {
     @Serializable
+    data object Home: Screen()
+
+    @Serializable
     data object TecnicoList: Screen()
 
     @Serializable
-    data class Tecnico(val tecnicoId: Int?) : Screen()
+    data class Tecnico(val tecnicoId: Int?): Screen()
+
+    @Serializable
+    data object PrioridadesList: Screen()
+
+    @Serializable
+    data class Prioridad(val prioridadId: Int?): Screen()
+
+    @Serializable
+    data object TicketsList: Screen()
+
+    @Serializable
+    data class Ticket(val ticketId: Int?): Screen()
 }
