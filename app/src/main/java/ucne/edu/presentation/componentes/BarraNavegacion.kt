5bfa6ee44614d6e9df267e7ec5e97fabeaff9e83
Yohualkis <Yohualkis99@gmail.com>
2025-05-22 09:48:50 -0400

    Se quitaron imports que no se usan

--- app/src/main/java/ucne/edu/presentation/componentes/BarraNavegacion.kt
+++ app/src/main/java/ucne/edu/presentation/componentes/BarraNavegacion.kt
@@ -13,11 +13,8 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Build
 import androidx.compose.material.icons.filled.ConfirmationNumber
-import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.PriorityHigh
-import androidx.compose.material.icons.filled.Warning
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
