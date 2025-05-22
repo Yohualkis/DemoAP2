204d4010894eccb6ea42f0240a6dd3cd2a30d79b
Yohualkis <Yohualkis99@gmail.com>
2025-05-22 09:51:24 -0400

    Se creo la pagina home

---
+++ app/src/main/java/ucne/edu/presentation/generalComposables/Home.kt
@@ -0,0 +1,107 @@
+package ucne.edu.presentation.generalComposables
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.ConfirmationNumber
+import androidx.compose.material.icons.filled.PriorityHigh
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import ucne.edu.data.local.entities.PrioridadEntity
+import ucne.edu.data.local.entities.TecnicoEntity
+import ucne.edu.data.local.entities.TicketEntity
+import ucne.edu.presentation.componentes.TopBarHome
+import ucne.edu.presentation.componentes.TotalEntidadesCard
+
+@Composable
+fun Home(
+    listaTecnicos: List<TecnicoEntity>,
+    listaTickets: List<TicketEntity>,
+    listaPrioridades: List<PrioridadEntity>,
+) {
+    Scaffold(
+        topBar = { TopBarHome() }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState()) // Esta linea es la que da el efecto de scroll jevi
+        ) {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                elevation = CardDefaults.cardElevation(4.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(24.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "Bienvenido a DemoAP2",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(
+                        text = "Esta es una aplicación de CRUD's de Programación Aplicada 2.",
+                        fontSize = 16.sp,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+
+            TotalEntidadesCard(
+                titulo = "Técnicos:",
+                icono = Icons.Filled.Build,
+                listaEntidades = listaTecnicos
+            )
+            TotalEntidadesCard(
+                titulo = "Prioridades:",
+                icono = Icons.Filled.PriorityHigh,
+                listaEntidades = listaPrioridades
+            )
+            TotalEntidadesCard(
+                titulo = "Tickets:",
+                icono = Icons.Filled.ConfirmationNumber,
+                listaEntidades = listaTickets
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewHome() {
+    var listaTecnicos: List<TecnicoEntity> = emptyList()
+    var listaTickets: List<TicketEntity> = emptyList()
+    var listaPrioridades: List<PrioridadEntity> = emptyList()
+    Home(
+        listaTecnicos = listaTecnicos,
+        listaTickets = listaTickets,
+        listaPrioridades = listaPrioridades
+    )
+}
