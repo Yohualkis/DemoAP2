a2c2c20843637303c517cd643cdc901f740762ca
Yohualkis <Yohualkis99@gmail.com>
2025-05-28 05:52:37 -0400

    Se cambio de lugar el screen de los mensajes, se elimino un argumento innecesario y se elimino un null promise

--- app/src/main/java/ucne/edu/presentation/navigation/GeneralNavHost.kt
+++ app/src/main/java/ucne/edu/presentation/navigation/GeneralNavHost.kt
@@ -5,7 +5,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
-import ucne.edu.presentation.generalComposables.Home
+import ucne.edu.presentation.componentes.Home
 import ucne.edu.presentation.mensajes.TicketComentariosScreen
 import ucne.edu.presentation.prioridades.DesplegarListadoDePrioridades
 import ucne.edu.presentation.prioridades.PrioridadScreen
@@ -16,7 +16,7 @@ import ucne.edu.presentation.tickets.TicketScreen
 
 @Composable
 fun GeneralNavHost(
-    navHostController: NavHostController
+    navHostController: NavHostController,
 ) {
     NavHost(
         navController = navHostController,
@@ -24,7 +24,18 @@ fun GeneralNavHost(
     ) {
 
         composable<Screen.Home> {
-            Home()
+            Home(nav = navHostController)
+        }
+
+        // MENSAJES
+        composable<Screen.Mensaje> { backStack ->
+            val args = backStack.toRoute<Screen.Mensaje>()
+            TicketComentariosScreen(
+                ticketId = args.ticketId,
+                goBack = {
+                    navHostController.navigateUp()
+                },
+            )
         }
 
         // TECNICOS
@@ -34,19 +45,6 @@ fun GeneralNavHost(
                 goToTecnico = { navHostController.navigate(Screen.Tecnico(it)) },
                 goBack = { navHostController.navigateUp() }
             )
-        }
-
-        // MENSAJES
-        composable < Screen.Mensaje>{ backStack ->
-            val args = backStack.toRoute<Screen.Mensaje>()
-            TicketComentariosScreen(
-                ticketId = args.ticketId!!,
-                goBack = {
-                    navHostController.navigateUp()
-                },
-                ticketAsunto = args.ticketAsunto,
-            )
-
         }
 
         composable<Screen.Tecnico> { backStack ->
